e82bf72160139a59a003f0da3260ec09d4640126
Yuta Okuma <y.dream.nest@icloud.com>
2022-08-28 18:49:46 +0900

    feat: add tide fish shell prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,12 @@
 # Color
 set -gx TERM xterm-256color
 
+# Theme
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g theme_hostname always
+
 # Aliases
 alias g git
 command -qv nvim && alias vim nvim
