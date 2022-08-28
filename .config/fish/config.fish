19600b5b02b66a87c3a7680a142182c873017a24
Yuta Okuma <y.dream.nest@icloud.com>
2022-08-28 15:55:19 +0900

    feat(config.fish): add some settings to config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,23 @@
-if status is-interactive
-  # Commands to run in interactive sessions can go here
-  set PATH /opt/homebrew/bin $PATH
-  set -x PATH $HOME/.anyenv/bin $PATH
-  eval (anyenv init - | source)
-end
+# Color
+set -gx TERM xterm-256color
+
+# Aliases
+alias g git
+command -qv nvim && alias vim nvim
+command -qv nvim && alias vi nvim
+
+set -gx EDITOR nvim
+
+set -gx PATH bin $PATH
+set -gx PATH ~/bin $PATH
+set -gx PATH ~/.local/bin $PATH
+
+# Homebrew
+set PATH /opt/homebrew/bin $PATH
+
+# anyenv
+set -x PATH $HOME/.anyenv/bin $PATH
+eval (anyenv init - | source)
 
 # exa
 if type -q exa
