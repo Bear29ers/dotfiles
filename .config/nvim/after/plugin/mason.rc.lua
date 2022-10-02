5dfca91b57aa2eadbbf24f5a0635b2f547513202
Yuta Okuma <y.dream.nest@icloud.com>
2022-10-02 16:39:51 +0900

    feat: add some configuration in null-ls

--- .config/nvim/after/plugin/mason.rc.lua
+++ .config/nvim/after/plugin/mason.rc.lua
@@ -1,11 +1,18 @@
 local status, mason = pcall(require, 'mason')
 if (not status) then return end
 
-local status2, lspconfig = pcall(require, 'mason-lspconfig')
+local status2, mason_lspconfig = pcall(require, 'mason-lspconfig')
 if (not status2) then return end
 
-mason.setup({})
+local status3, lspconfig = pcall(require, 'lspconfig')
+if (not status3) then return end
 
-lspconfig.setup {
-  ensure_installed = { 'sumneko_lua', 'tailwindcss' }
-}
+mason.setup({
+  ui = {
+    icons = {
+      package_installed = "✓",
+      package_pending = "➜",
+      package_uninstalled = "✗"
+    }
+  }
+})
