18bfff0c76acea5ef41b12591ba2f76fb4f200aa
Yuta Okuma <y.dream.nest@icloud.com>
2023-04-05 09:30:42 +0900

    config: :gear: Add volar (vue) lsp config

--- .config/nvim/plugin/lspconfig.lua
+++ .config/nvim/plugin/lspconfig.lua
@@ -65,6 +65,31 @@ nvim_lsp.lua_ls.setup {
   },
 }
 
+local util = require 'lspconfig.util'
+local function get_typescript_server_path(root_dir)
+  local global_ts = '$HOME/.anyenv/envs/nodenv/versions/18.13.0/lib/typescript'
+  -- Alternative location if installed as root:
+  -- local global_ts = '/usr/local/lib/node_modules/typescript/lib'
+  local found_ts = ''
+  local function check_dir(path)
+    found_ts = util.path.join(path, 'node_modules', 'typescript', 'lib')
+    if util.path.exists(found_ts) then
+      return path
+    end
+  end
+  if util.search_ancestors(root_dir, check_dir) then
+    return found_ts
+  else
+    return global_ts
+  end
+end
+
+require 'lspconfig'.volar.setup {
+  on_new_config = function(new_config, new_root_dir)
+    new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
+  end,
+}
+
 nvim_lsp.tailwindcss.setup {
   on_attach = on_attach,
   capabilities = capabilities
