9eeda187d0840f34c3a18781b69440bb39e70cea
bear27leap <yu.okuma@gakken.co.jp>
2023-05-17 08:47:00 +0900

    config: :gear: Change null-ls configuration

--- .config/nvim/plugin/null-ls.rc.lua
+++ .config/nvim/plugin/null-ls.rc.lua
@@ -17,11 +17,11 @@ end
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.prettierd,
-    null_ls.builtins.diagnostics.eslint_d.with({
+    null_ls.builtins.diagnostics.eslint.with({
       diagnostics_format = "[eslint] #{m}\n(#{c})",
     }),
     null_ls.builtins.diagnostics.fish,
-    null_ls.builtins.code_actions.eslint_d,
+    null_ls.builtins.code_actions.eslint,
   },
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
