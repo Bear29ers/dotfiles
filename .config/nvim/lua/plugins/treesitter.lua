4a77bb63ee61dbcf539759deb1563b3cf04ad51b
Yuta Okuma <y.dream.nest@icloud.com>
2023-11-19 17:37:12 +0900

    :wrench: update treesitter autotag settings

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,16 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "windwp/nvim-ts-autotag",
+    {
+      "windwp/nvim-autopairs",
+      event = "InsertEnter",
+      opts = {},
+    },
+  },
   opts = {
+    highlight = { enable = true },
+    indent = { enable = true },
     ensure_installed = {
       "bash",
       "css",
@@ -27,4 +37,25 @@ return {
       "yaml",
     },
   },
+  config = function(_, opts)
+    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+    local cmp = require("cmp")
+    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
+    local autopairs = require("nvim-autopairs")
+    autopairs.setup({
+      check_ts = true,
+      ts_config = {
+        javascript = { "template_string" },
+      },
+    })
+
+    require("nvim-treesitter.configs").setup(opts)
+    require("nvim-ts-autotag").setup({
+      enable = true,
+      enable_rename = true,
+      enable_close = true,
+      enable_close_on_slash = true,
+    })
+  end,
 }
