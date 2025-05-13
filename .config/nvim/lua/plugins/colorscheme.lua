8927ccea30bdb066c19970f63c9e993084e9b49e
Bear29ers <y.dream.nest@icloud.com>
2025-05-13 18:39:58 +0900

    :wrench: conf: update config for colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,15 @@
 return {
   -- add onedark
-  { "navarasu/onedark.nvim" },
+  {
+    "navarasu/onedark.nvim",
+    opts = {
+      style = "darker",
+    },
+    config = function(_, opts)
+      require("onedark").setup(opts)
+      require("onedark").load()
+    end,
+  },
 
   -- Configure LazyVim to load onedark
   {
