1bae0f0a62881eb3e4fd6335e2f5c0c31fc99d68
Yuta Okuma <y.dream.nest@icloud.com>
2025-06-07 00:07:39 +0900

    :wrench: conf: add window resize keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- Resize window with Meta (Alt) + hjkl
+map("n", "<M-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<M-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<M-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+map("n", "<M-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
