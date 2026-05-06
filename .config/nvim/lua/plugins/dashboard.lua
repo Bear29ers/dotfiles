e4d7ab188c2aeae9c93270366c5019f073553f8b
Yuta Okuma <y.dream.nest@icloud.com>
2026-05-06 17:22:57 +0900

    :lipstick: ui: fix dashboard logo

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -1,30 +1,30 @@
 local logo = [[
-                    ██████        ██████████          ██████
-                  ████████████████          ██████████████████
-                ████████████                        ████████████
-              ████████████                            ████████████
-              ██████████                                ██████████
-              ████████                                    ████████
-                ████        ██████            ██████        ████
-                  ██      ████████            ████████      ██
-                  ██    ██████  ██            ██  ██████    ██
-                  ██    ██████████            ██████████    ██
-                  ██    ██████████  ████████  ██████████    ██
-                    ██  ████████    ████████    ████████  ██
-                    ██    ████                    ████    ██
-                      ██                                ██
-                        ██                            ██
-                          ████                    ████
-                            ████████████████████████
-                            ████████        ████████
-                            ██  ████        ████  ██
-                            ██                    ██
-                            ██                    ██
-                              ████████████████████
-                              ██████        ██████
-                                ████        ████
-
-                                  [bear29ers]
+                        ██████        ██████████          ██████                        
+                      ████████████████          ██████████████████                      
+                    ████████████                        ████████████                    
+                  ████████████                            ████████████                  
+                  ██████████                                ██████████                  
+                  ████████                                    ████████                  
+                    ████        ██████            ██████        ████                    
+                      ██      ████████            ████████      ██                      
+                      ██    ██████  ██            ██  ██████    ██                      
+                      ██    ██████████            ██████████    ██                      
+                      ██    ██████████  ████████  ██████████    ██                      
+                        ██  ████████    ████████    ████████  ██                        
+                        ██    ████                    ████    ██                        
+                          ██                                ██                          
+                            ██                            ██                            
+                              ████                    ████                              
+                                ████████████████████████                                
+                                ████████        ████████                                
+                                ██  ████        ████  ██                                
+                                ██                    ██                                
+                                ██                    ██                                
+                                  ████████████████████                                  
+                                  ██████        ██████                                  
+                                    ████        ████                                    
+                                                                                        
+                                      [bear29ers]                                       
 ]]
 
 return {
@@ -34,15 +34,20 @@ return {
       preset = {
         header = logo,
         keys = {
-          { icon = " ", key = "f", desc = "Find file",       action = ":lua Snacks.dashboard.pick('files')" },
-          { icon = " ", key = "n", desc = "New file",        action = ":ene | startinsert" },
-          { icon = " ", key = "r", desc = "Recent files",    action = ":lua Snacks.dashboard.pick('oldfiles')" },
-          { icon = " ", key = "g", desc = "Find text",       action = ":lua Snacks.dashboard.pick('live_grep')" },
-          { icon = " ", key = "c", desc = "Config",          action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = " ", key = "f", desc = "Find file", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New file", action = ":ene | startinsert" },
+          { icon = " ", key = "r", desc = "Recent files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = " ", key = "g", desc = "Find text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          {
+            icon = " ",
+            key = "c",
+            desc = "Config",
+            action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+          },
           { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-          { icon = " ", key = "x", desc = "Lazy Extras",     action = ":LazyExtras" },
-          { icon = "󰒲 ", key = "l", desc = "Lazy",           action = ":Lazy" },
-          { icon = " ", key = "q", desc = "Quit",            action = ":qa" },
+          { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
         },
       },
     },
