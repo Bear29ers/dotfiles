66ae9a538df6cb413c01e04f03db644635b448db
Yuta Okuma <y.dream.nest@icloud.com>
2026-05-24 11:57:23 +0900

    :wrench: conf: change dashboard logo fg color

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -1,3 +1,10 @@
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_hl(0, "SnacksDashboardHeader", { fg = "#abb2bf" })
+  end,
+})
+
 local logo = [[
                         ██████        ██████████          ██████                        
                       ████████████████          ██████████████████                      
